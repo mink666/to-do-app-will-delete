4f6cd325cf56baa7677a4d0ec4d71579b3c82988
Your Name <your-email@example.com>
2025-03-14 19:25:18 +0700

    session-1-route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "Hello World";
+}); //simple route
+
+Route::get('/about/{name}', function ($name) {
+    return "Hello ". $name;
+}); //passing parameter
+
+Route::get('/greeting', function () {
+    return "Greetings";
+})->name('greeting'); //set name for function
+
+Route::get('/hello', function () {
+    return redirect()->route('/greeting');
+}); //redirect to greeting
+
+Route::get('/home', function () {
+    return view('Welcome');
+}); //return view
