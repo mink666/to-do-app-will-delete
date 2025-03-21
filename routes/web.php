4eb9ad5ce4a00ab59e3db2dcc4942184b39c5a9e
Your Name <your-email@example.com>
2025-03-21 19:38:04 +0700

    Session 5 commit

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,34 @@
 <?php
-
+use Illuminate\Http\Request;
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {
-    $tasks = Task::latest()->get();
+    $tasks = Task::latest('id')->get();
     return view('index', ['tasks' => $tasks]);
-})->name('index');
+})->name('tasks.index');
+
+Route::get('/task/create', function () {
+    return view('create');
+})->name('tasks.create');
 
 Route::get('/task/{id}', function ($id) {
     $task = Task::findOrFail($id);
     return view('detail', ['task' => $task]);
 })->name('tasks.detail');
+
+Route::post('/task', function (Request $request) {
+    // dd($request->all());
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required|max:255',
+        'long_description' => 'required|max:255',
+    ]);
+    $task = new Task();
+    $task->title = $request->title;
+    $task->description = $request->description;
+    $task->long_description = $request->long_description;
+    $task->completed = false;
+    $task->save();
+    return redirect()->route('tasks.index');
+})->name('tasks.create');
