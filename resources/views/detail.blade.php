8a5c2c2b5a4dc6a1ba47cfc9d09a860f7cfcc89b
Your Name <your-email@example.com>
2025-03-21 18:48:43 +0700

    Add session 4

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,21 +1,12 @@
-<?php
+@extends('layouts.app')
 
-use Illuminate\Support\Facades\Route;
-?>
+@section('title', $task->title)
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $task->title }}</title>
-</head>
-<body>
-    <h1>{{ $task->title }}</h1>
+@section('content')
     <p>{{ $task->description }}</p>
     <p>{{ $task->long_description }}</p>
     <p>{{ $task->status ? 'DONE' : 'UNCOMPLETE' }}</p>
     <a href="{{ route('index') }}">Back to menu</a>
-</body>
-</html>
+
+
+@endsection
