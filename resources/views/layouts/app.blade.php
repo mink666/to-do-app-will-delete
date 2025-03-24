1a2eb332ca50dc5edabf2666ee27d9be09db32a4
Your Name <your-email@example.com>
2025-03-24 18:27:24 +0700

    comit 6

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <style>
+        @yield('styles')
+        .alert-succes{
+            color: green;
+        }
+    </style>
 </head>
 <body>
     <h1>@yield('title')</h1>
     @yield('content')
+    @if (session()->has('success'))
+        <div class="alert alert-success">{{ session()->get('success') }}</div>
+    @endif
 </body>
 </html>
