274956e3dfd25a6aac320182e34c85500a844288
Your Name <your-email@example.com>
2025-03-24 19:47:26 +0700

    Define tasks.delete route and update TaskController

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Example-app')
 
 @section('content')
+    <a href="{{ route('tasks.create') }}">Create task</a>
     <table border="1">
         <tr>
             <th>Id</th>
@@ -16,9 +17,15 @@
                     <td>{{ $task->id }}</td>
                     <td>{{ $task->title }}</td>
                     <td>{{ $task->status ? 'DONE' : 'UNCOMPLETE' }}</td>
-                    <td><a href="{{ route('tasks.detail', ['id' => $task->id]) }}">Details</a></td>
-                    <td><a href="">Edit</a></td>
-                    <td><a href="">Delete</a></td>
+                    <td><a href="{{ route('tasks.detail', ['id' => $task->id]) }}">Details</a>
+                    <td><a href="{{ route('tasks.edit', ['id' => $task->id]) }}">Edit</a></td>
+                    <td>
+                        <form action ="{{ route('tasks.delete', ['id' => $task->id]) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Delete</button>
+                        </form>
+                    </td>
                 </tr>
             @endforeach
         @else
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 use Illuminate\Http\Request;
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Requests\TaskRequest;
 
 Route::get('/', function() {
     $tasks = Task::latest('id')->get();
@@ -17,18 +19,43 @@ Route::get('/task/{id}', function ($id) {
     return view('detail', ['task' => $task]);
 })->name('tasks.detail');
 
-Route::post('/task', function (Request $request) {
-    // dd($request->all());
+Route::post('/task', function (TaskRequest $request) {
     $data = $request->validate([
         'title' => 'required|max:255',
         'description' => 'required|max:255',
         'long_description' => 'required|max:255',
     ]);
     $task = new Task();
-    $task->title = $request->title;
-    $task->description = $request->description;
-    $task->long_description = $request->long_description;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
     $task->completed = false;
     $task->save();
     return redirect()->route('tasks.index');
-})->name('tasks.create');
+})->name('tasks.store');
+
+Route::get('/tasks/{id}/edit', function ($id) {
+    $task = Task::findOrFail($id);
+    return view('edit', ['task' => $task]);
+})->name('tasks.edit');
+
+Route::put('/task/{id}', function (Request $request, $id) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required|max:255',
+        'long_description' => 'required|max:255',
+    ]);
+    $task = Task::findOrFail($id);
+    $task->title = $request->title;
+    $task->description = $request->description;
+    $task->long_description = $request->long_description;
+    $task->completed = $request->has('completed');
+    $task->save();
+    return redirect()->route('tasks.index')->with('message', 'Task updated successfully!');
+})->name('tasks.update');
+
+Route::delete('/task/{id}', function ($id) {
+    $task = Task::findOrFail($id);
+    $task->delete();
+    return redirect()->route('tasks.index')->with('message', 'Task deleted successfully!');
+})->name('tasks.delete');
