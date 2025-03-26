fe218b4c2c89a505413250e90a6639f1100340bc
Your Name <your-email@example.com>
2025-03-26 19:24:56 +0700

    Fix task creation form and validation logic

--- routes/web.php
+++ routes/web.php
@@ -1,61 +1,62 @@
 <?php
-use Illuminate\Http\Request;
-use App\Models\Task;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController;
-use App\Http\Requests\TaskRequest;
 
-Route::get('/', function() {
-    $tasks = Task::latest('id')->get();
-    return view('index', ['tasks' => $tasks]);
+use App\Http\Requests\TaskRequest;
+use Illuminate\Support\Facades\Route;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/', function () {
+    $tasks = Task::latest()->paginate();
+    return view('index',['tasks' => $tasks]);
 })->name('tasks.index');
 
-Route::get('/task/create', function () {
-    return view('create');
-})->name('tasks.create');
+Route::view('/tasks/create', 'create')->name('tasks.create');
 
-Route::get('/task/{id}', function ($id) {
-    $task = Task::findOrFail($id);
-    return view('detail', ['task' => $task]);
-})->name('tasks.detail');
+Route::get('/tasks/{task}', function (Task $task) {
+    return view('show',['task' => $task]);
+})->name('tasks.show');
 
-Route::post('/task', function (TaskRequest $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required|max:255',
-        'long_description' => 'required|max:255',
-    ]);
-    $task = new Task();
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->completed = false;
-    $task->save();
-    return redirect()->route('tasks.index');
+Route::post('/tasks', function (TaskRequest $request) {
+    // dd('We have store route');
+    // dd($request->all());
+    $task = Task::create($request->validated());
+    return redirect()->route('tasks.show',['task' => $task->id])
+        ->with('success','Task created successfully');
 })->name('tasks.store');
 
-Route::get('/tasks/{id}/edit', function ($id) {
-    $task = Task::findOrFail($id);
-    return view('edit', ['task' => $task]);
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', [
+        'task' => $task
+    ]);
 })->name('tasks.edit');
 
-Route::put('/task/{id}', function (Request $request, $id) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required|max:255',
-        'long_description' => 'required|max:255',
-    ]);
-    $task = Task::findOrFail($id);
-    $task->title = $request->title;
-    $task->description = $request->description;
-    $task->long_description = $request->long_description;
-    $task->completed = $request->has('completed');
-    $task->save();
-    return redirect()->route('tasks.index')->with('message', 'Task updated successfully!');
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+    // dd('We have store route');
+    // dd($request->all());
+    $task->update($request->validated());
+    return redirect()->route('tasks.show',['task' => $task->id])
+        ->with('success','Task created successfully');
 })->name('tasks.update');
 
-Route::delete('/task/{id}', function ($id) {
-    $task = Task::findOrFail($id);
+Route::fallback(function () {
+    return 'Still got somewhere!';
+});
+
+Route::delete('/tasks/{task}', function (Task $task) {
     $task->delete();
-    return redirect()->route('tasks.index')->with('message', 'Task deleted successfully!');
-})->name('tasks.delete');
+    return redirect()->route('tasks.index')
+        ->with('success','Task deleted successfully');
+})->name('tasks.destroy');
+
+Route::put('/tasks/{task}/toggle', function (Task $task) {
+    // $task->toggleComplete(); -> Sau khi đóng gói ở model, có thể dùng dể thay thế code phía dưới
+    $task->completed = !$task->completed;
+    $task->save();
+    return redirect()->back()
+        ->with('success','Task updated successfully');
+})->name('tasks.toggle-complete');
